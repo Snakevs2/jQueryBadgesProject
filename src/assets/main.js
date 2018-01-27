75eee85da54359b05babb7b326a2aa11665ff18a
Snakevs2 <normcrazy@yahoo.com>
2018-01-27 17:03:02 -0600

    test

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,17 @@
 $(function() {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/rittera13.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      json = response;
+      $.each(json.courses.completed, function(index, value) {
+        $("#badges").append("<div class='course' id='course_" + index + "'></div>");
+        $("#course_" + index).append("<h3>" + value.title + "</h3>");
+        $("#course_" + index).append("<img src='" + value.badge + "'/>");
+        $("#course_" + index).append("<a class='btn btn-primary' href='" + value.url + "'>See Courses</a>");
 
-  // your code will go here
+      });
 
+    }
+  });
 });
